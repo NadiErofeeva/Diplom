a3d91033162f290a38393a8e5c2ac02c31dd52c4
Nadi Erofeeva <n.erofeeva72@gmail.com>
2023-04-11 23:42:03 +0500

    index

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import scroll from './modules/scroll';
+import btnUp from './modules/btnUp';
+import accordion from './modules/accordion';
+import modal from './modules/modal';
+
+scroll();
+btnUp();
+accordion();
+modal();
