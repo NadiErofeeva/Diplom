dc326c4e870a45f105cd6e2a5d8c762349ca12a6
Nadi Erofeeva <n.erofeeva72@gmail.com>
2023-04-12 22:57:52 +0500

    build(scroll fix, slider)

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import scroll from './modules/scroll';
 import btnUp from './modules/btnUp';
 import accordion from './modules/accordion';
 import modal from './modules/modal';
+import slider from './modules/slider';
 
 scroll();
 btnUp();
 accordion();
 modal();
+slider();
