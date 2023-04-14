2002a0aebc655fe51fbade0aadb06f0f97267e45
Nadi Erofeeva <n.erofeeva72@gmail.com>
2023-04-15 02:05:09 +0500

    build(sendForm, valid, carousel)

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import btnUp from './modules/btnUp';
 import accordion from './modules/accordion';
 import modal from './modules/modal';
 import slider from './modules/slider';
+import { carousel } from './modules/carousel';
+import sendForm from './modules/sendForm';
+import valid from './modules/valid';
 
 scroll();
 btnUp();
 accordion();
 modal();
 slider();
+carousel();
+sendForm();
+valid();
